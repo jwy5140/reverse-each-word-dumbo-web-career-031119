d654bd76fe365b5b629d445b1a42b3c342b36983
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 21:33:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(str)
   arr = str.split(" ")
   arr.each {|x| x.reverse!}
   arr.join(" ")
+  
   ############ using collect 
   
   arr2 = (str.split(" ")).collect{|x| x.reverse!}
